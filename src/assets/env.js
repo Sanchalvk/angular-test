23e602055ecd9e30e6af169277cbb1d7cfb226e2
Sanchalvk <150319103+Sanchalvk@users.noreply.github.com>
2025-10-29 23:50:22 +0530

    Update and rename env.js to env.template.js

--- src/assets/env.js
+++
@@ -1,4 +0,0 @@
-(function (window) {
-  window.__env = window.__env || {};
-  window.__env.apiUrl = "$API_URL"; // placeholder for Docker to replace
-})(this);
---
+++ src/assets/env.template.js
@@ -0,0 +1,5 @@
+(function (window) {
+  window.__env = window.__env || {};
+  // Placeholder for Docker to replace at runtime
+  window.__env.apiUrl = "$API_URL";
+})(this);
