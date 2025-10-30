df8f3c6ba475b4c5be4cbc87105b9e63ed4f4193
Sanchalvk <150319103+Sanchalvk@users.noreply.github.com>
2025-10-30 15:58:25 +0530

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ echo ">>> Generating env.js with API_URL=$API_URL"
 
 # if template exists, generate env.js; else leave existing env.js
 TEMPLATE="/usr/share/nginx/html/my-login-app/assets/env.template.js"
-TARGET="/usr/share/nginx/html/my-login-app/assets/env.js"
+TARGET="/usr/share/nginx/html/my-login-app/assets/env.template.js"
 
 if [ -f "$TEMPLATE" ]; then
   envsubst '$API_URL' < "$TEMPLATE" > "$TARGET"
