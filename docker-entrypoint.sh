cca0d105f2070220c54ad8f41b63e384ad62d7c5
Sanchalvk <150319103+Sanchalvk@users.noreply.github.com>
2025-10-30 16:00:46 +0530

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
-set -e
 
-# default fallback if API_URL not set in compose
-API_URL=${API_URL:-"http://localhost:8081/v1"}
-
-echo ">>> Generating env.js with API_URL=$API_URL"
-
-# if template exists, generate env.js; else leave existing env.js
-TEMPLATE="/usr/share/nginx/html/my-login-app/assets/env.template.js"
-TARGET="/usr/share/nginx/html/my-login-app/assets/env.template.js"
-
-if [ -f "$TEMPLATE" ]; then
-  envsubst '$API_URL' < "$TEMPLATE" > "$TARGET"
-else
-  echo "No env.template.js found at $TEMPLATE — leaving env.js unchanged if present"
-fi
+# Replace placeholder in env.template.js
+echo "🔧 Replacing API_URL with: $API_URL"
+sed -i "s|\$API_URL|${API_URL:-http://localhost:8081/v1}|g" /usr/share/nginx/html/assets/env.template.js
 
 exec "$@"
